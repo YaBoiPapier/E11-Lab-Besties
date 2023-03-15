d24417736eef97a5b5b0a42a80fbfe349fb82d38
Jackson Avedon <70713533+1orbit@users.noreply.github.com>
2023-03-15 15:55:00 -0700

    Update Week9Lab.py

--- Week9Lab.py
+++ Week9Lab.py
@@ -2,22 +2,28 @@
  
 import RPi.GPIO as GPIO
 import time
-import datetime
- 
-def my_callback(channel):
-    if GPIO.input(channel) == GPIO.HIGH:
-        print('\n▼  at ' + str(datetime.datetime.now()))
-    else:
-        print('\n ▲ at ' + str(datetime.datetime.now()))
- 
-try:
-    GPIO.setmode(GPIO.BCM)
-    GPIO.setup(6, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-    GPIO.add_event_detect(6, GPIO.BOTH, callback=my_callback)
- 
-    message = raw_input('\nPress any key to exit.\n')
- 
-finally:
-    GPIO.cleanup()
- 
-print("Goodbye!")
+
+#pin setup
+count = 0
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(23, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+#define callback
+def pulse_counter(channel):
+    global count
+    count+= 1
+    print(time.time())
+
+#execute callback
+GPIO.add_event_detect(23, GPIO.FALLING, callback=pulse_counter)
+
+while True:
+    try:
+        count = 0
+        time.sleep(60)
+        print("Number of counts in last minute:", count)
+        message = raw_input('\nPress any key to exit:\n')
+    except KeyboardInterrupt:
+        GPIO.cleanup()
+      
+print("Goodbye World!")
